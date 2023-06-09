6e90948a1b6bed3e058d78627f3eaeef5a71e48f
Robert Orlikowski <38508936+robert72127@users.noreply.github.com>
2023-06-09 15:38:26 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,33 +9,32 @@ def save_images(images, path, **kwargs):
     print(path)
     im.save(path)
 
-class Obscure_images:
-  def __init__(self, X, rectangles=10, max_rect=0.3):
-    self.imgs = X.clone()
-    self.img_count = self.imgs.shape[0]
-    self.img_size = self.imgs.shape[2]
-    self.max_rect = int(max_rect * self.img_size)
-    self.rectangles = rectangles
-    self.obscure_imgs_tensor()
-  
-  def obscure_image(self,x, x_s,x_e,y_s,y_e):
-    if x_e < x_s:
-      x_e,x_s = x_s,x_e
-    if y_e < y_s:
-      y_e,y_s = y_s,y_e
-    x[:, y_s:y_e, x_s:x_e] = .5
-    return x
+class Obscure(object):
+    """
+    Add extra 3 channels to image that will represent obscured image
+    """
+    def __init__(self, img_size, rectangles=10, max_rect=0.3):
+        self.img_size = img_size
+        self.max_rect_area = int(max_rect * self.img_size)
+        self.rectangles = rectangles
 
-  def random_obscure(self,x):
-    cnt = 10
-    for i in range(cnt):
-      x_e,y_e = random.sample(range(0, self.img_size), 2)
-      x_s = random.randint(x_e-self.max_rect, x_e)
-      y_s = random.randint(y_e-self.max_rect,y_e)
-      x = self.obscure_image(x, x_s,x_e,y_s,y_e)
-    return x
-  
-  def obscure_imgs_tensor(self):
-    img_cnt = imgs.shape[0]
-    for i in range(self.img_count):
-      self.imgs[i] = self.random_obscure(self.imgs[i])
+    def __call__(self, img):
+        obscured = self.random_obscure(img.clone())
+        return torch.concat((img,obscured), dim=0)
+
+    def obscure_image(self,x, x_s,x_e,y_s,y_e):
+      if x_e < x_s:
+        x_e,x_s = x_s,x_e
+      if y_e < y_s:
+        y_e,y_s = y_s,y_e
+      x[:, y_s:y_e, x_s:x_e] = .5
+      return x
+
+    def random_obscure(self,x):
+      cnt = self.rectangles
+      for i in range(cnt):
+        x_e,y_e = random.sample(range(0, self.img_size), 2)
+        x_s = random.randint(x_e-self.max_rect_area, x_e)
+        y_s = random.randint(y_e-self.max_rect_area,y_e)
+        x = self.obscure_image(x, x_s,x_e,y_s,y_e)
+      return x
