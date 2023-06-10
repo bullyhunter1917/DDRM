e0f5d456692708cba7a4f5a86cfc882cb0a7d7c7
Robert Orlikowski <38508936+robert72127@users.noreply.github.com>
2023-06-10 16:45:35 +0200

    forgot to import sys

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import torch_xla.core.xla_model as xm
 import torch_xla.distributed.parallel_loader as pl
 import torch_xla.distributed.xla_multiprocessing as xmp
 import torch_xla.utils.utils as xu
+import sys
 
 LSUN_DIR = '' # will fill later, after uploading
 
